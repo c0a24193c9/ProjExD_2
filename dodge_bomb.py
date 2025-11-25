ee88493763bba0861c6dfbd17560f1985c06ecaf
hashimoto ayane <c0a24193c9@edu.teu.ac.jp>
2025-11-25 14:40:46 +0900

    練習３：画面外に出ないように

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = {                    #練習１
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横方向、縦方向）
+    画面内ならTrue,画面外ならFalse
+    """
+    yoko,tate=True,True
+    if rct.left < 0 or WIDTH < rct.right:#横方向判定
+        yoko=False
+    if rct.top<0 or HEIGHT < rct.bottom:#縦方向判定
+        tate=False
+    return yoko,tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -51,7 +65,16 @@ def main():
                 sum_mv[0]+=mv[0] #横方向の移動量
                 sum_mv[1]+=mv[1] #縦方向の移動量
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko, tate =check_bound(bb_rct)
+        if not yoko: #横はみだし
+            vx *= -1
+        if not tate: #縦はみだし
+            vy *= -1
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
